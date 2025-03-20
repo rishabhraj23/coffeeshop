f0d83b308932a4356a0994dd0e3c7f94579cd419
Rishabh Raj <brotherslife22@gmail.com>
2025-03-20 17:14:15 +0000

    added conf files

--- coffeeshop/settings.py
+++ coffeeshop/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-d7$7#i)k7sp5h_ws481=&n^z*9&h3jni%o2x)8*ptdtjqc3$6n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -114,8 +114,15 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
+import os 
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS =[
+    os.path.join(BASE_DIR, 'static')
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
