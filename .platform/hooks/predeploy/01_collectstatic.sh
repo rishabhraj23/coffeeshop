a8d94ba61191ebae62f2d0fc1bb49cdfba79fb73
Rishabh <brotherslife22@gmail.com>
2025-03-30 20:07:44 +0100

    back to original conf

--- .platform/hooks/predeploy/01_collectstatic.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Activate the virtual environment
-source /var/app/venv/*/bin/activate
-
-# Navigate to the application directory
-cd /var/app/current
-
-# Run collectstatic without input prompt
-python3 manage.py collectstatic --noinput
